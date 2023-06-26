dc0ce1d2e70eac610d02b43b077082b4e211d700
Carlos Pajuelo Rojo <carlos@x230>
2023-06-26 10:02:01 +0200

    Fix variable name

--- spanish-holidays.el
+++ spanish-holidays.el
@@ -27,7 +27,7 @@
 ;; (customize-set-variable 'holiday-bahai-holidays nil)
 ;; (customize-set-variable 'holiday-hebrew-holidays nil)
 ;; (customize-set-variable 'holiday-islamic-holidays nil)
-;; (setq calendar-holidays (append calendar-holidays holiday-spanish-holidays))
+;; (setq calendar-holidays (append calendar-holidays spanish-holidays))
 ;;
 
 ;;; Code:
