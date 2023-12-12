cf892251468e6ba38c259f759bf31e7f9ad1c036
carlos <carlos@x230>
2023-12-12 22:07:09 +0100

    Updated holidays for 2024

--- spanish-holidays.el
+++ spanish-holidays.el
@@ -37,19 +37,18 @@
   (require 'holidays))
 
 (defvar spanish-holidays
-  '((holiday-fixed 1 2 "Lunes siguiente al Año Nuevo")
+  '((holiday-fixed 1 1 "Año Nuevo")
     (holiday-fixed 1 6 "Epifanía del Señor")
-    (holiday-fixed 4 6 "Jueves Santo")
-    (holiday-fixed 4 7 "Viernes Santo")
+    (holiday-fixed 4 28 "Jueves Santo")
+    (holiday-fixed 4 29 "Viernes Santo")
     (holiday-fixed 5 1 "Fiesta del Trabajo")
     (holiday-fixed 7 25 "Santiago Apóstol")
     (holiday-fixed 8 15 "Asunción de la Virgen")
     (holiday-fixed 10 12 "Fiesta Nacional de España")
     (holiday-fixed 11 1 "Todos los Santos")
     (holiday-fixed 12 6 "Día de la Constitución Española")
-    (holiday-fixed 12 8 "Día de la Inmaculada Concepción")
+    (holiday-fixed 12 9 "Día siguiente a la Inmaculada Concepción")
     (holiday-fixed 12 25 "Natividad del Señor")))
-
 
 (provide 'spanish-holidays)
 
